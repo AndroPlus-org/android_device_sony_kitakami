b8dfb943a6710b973d0add293ffb02a8e621e593
WedyDQ10 <hosigumayuugi@gmail.com>
2015-09-11 22:11:03 +0900

    merge dl12345 and change

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,45 +61,33 @@ BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
 
 TARGET_RECOVERY_FSTAB = device/sony/kitakami/rootdir/twrp.fstab
 
-TARGET_SCREEN_HEIGHT := 1280
-TARGET_SCREEN_WIDTH := 800
-DEVICE_RESOLUTION := 1280x800
-
-TARGET_RECOVERY_PIXEL_FORMAT := "RGB_555"
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBA_8888"
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
 
 # TWRP flags
+RECOVERY_VARIANT=twrp
+DEVICE_RESOLUTION := 1600x2560
 TW_THEME := landscape_hdpi
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
 RECOVERY_SDCARD_ON_DATA := true
 TW_HAS_NO_RECOVERY_PARTITION := true
 TW_FLASH_FROM_STORAGE := true
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TW_DEFAULT_EXTERNAL_STORAGE := true
-# TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_JB_CRYPTO := true
 TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/bootdevice/by-name/userdata"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p43"
 TW_CRYPTO_MNT_POINT := "/data"
 TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
 TW_CRYPTO_FS_FLAGS := "0x00000406"
 TW_CRYPTO_KEY_LOC := "footer"
 TW_INCLUDE_FUSE_EXFAT := true
-# TW_BOARD_CUSTOM_GRAPHICS := ../../../device/sony/kitakami-common/recovery/twrpgraphics.c
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 4095
 TW_NO_USB_STORAGE := true
 TW_NO_SCREEN_BLANK := true
-#TARGET_USERIMAGES_USE_F2FS := true
-#TW_EXCLUDE_DEFAULT_USB_INIT := true
-#TW_NO_CPU_TEMP := true
-#TW_INPUT_BLACKLIST := "accelerometer"
-#TW_NEW_ION_HEAP := true
-#TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
-#TW_SCREEN_BLANK_ON_BOOT := true
-TW_IGNORE_ABS_MT_TRACKING_ID := true
 
 BOARD_USES_QC_TIME_SERVICES := true
 
@@ -162,13 +150,31 @@ endif
 BUILD_KERNEL := true
 -include vendor/sony/kernel/KernelConfig.mk
 #TARGET_KERNEL_SOURCE := kernel/sony/msm
-#TARGET_KERNEL_ARCH := arm64
-#TARGET_KERNEL_HEADER_ARCH := arm64
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-#TARGET_KERNEL_CONFIG := aosp_kitakami_karin_defconfig
-#TARGET_USES_UNCOMPRESSED_KERNEL := true
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_HEADER_ARCH := arm64
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CONFIG := aosp_kitakami_karin_defconfig
+TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy/sepolicy.mk
 
-#BOARD_SEPOLICY_DIRS += device/sony/kitakami/sepolicy
+BOARD_SEPOLICY_DIRS += \
+    device/sony/kitakami/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    addrsetup.te \
+    device.te \
+    file.te \
+    property.te \
+    sct.te \
+    sensors.te \
+    service.te \
+    system_app.te \
+    tad.te \
+    ta_qmi.te \
+    thermanager.te \
+    timekeep.te \
+    file_contexts \
+    property_contexts \
+    service_contexts
